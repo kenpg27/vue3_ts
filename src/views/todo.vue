186caa1de043dcca88f121ec0de961dfb80765ff
Ken <396989681@qq.com>
2020-08-26 15:23:57 +0800

    vue3_base

---
+++ src/views/todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="add-box">
+    <input type="text" />
+    <button>添加</button>
+  </div>
+  <div class="list-box">
+      <div class="list-item">
+          待办1
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "todo",
+};
+</script>
+
+<style>
+</style>
