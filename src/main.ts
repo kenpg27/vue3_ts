9031dca42eb2f5b3f27780112008ec702f68e774
Ken <396989681@qq.com>
2020-08-26 14:53:12 +0800

    vue3.0_base_ts

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import router from './router/index'
+import store from './store/index'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.mount('#app')
